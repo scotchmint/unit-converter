a5bf352d5dec1ec9f4f41a5ce70073d54a064ea0
scotchmint <nullptr91@tuta.io>
2023-02-06 22:54:19 +0200

    Finish second section

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+// round numbers to 3 decimal
+// functions:
+// meters to feet = m * f
+// feets to meters = f / m
+// liters to gallons = l * g
+// gallons to liters = g / l
+// kilos to pounds = k * p
+// pounds to kilos = p / k
+
